6c871db27c2fe087f9d230fedbe08e5f299276aa
acpre <acpre@DESKTOP-H8R72AU>
2020-03-16 20:13:03 -0500

    move to my(Suganya) branch

---
+++ Selenium_Automation/src/corejava/GitDemoBranch.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class GitDemoBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Move code to Suganya Branch");
+	}
+
+}
--- Selenium_Automation/src/corejava/Loops.java
+++ Selenium_Automation/src/corejava/Loops.java
@@ -4,5 +4,6 @@ public class Loops {
 
 	public static void main(String[] args) {
 		System.out.println("testing");
+		System.out.println("Added new line for demo");
 	}
 }
