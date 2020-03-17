0e8c1d9245b18bbc1a858a0d5c8e3f668239b6b1
acpre <acpre@DESKTOP-H8R72AU>
2020-03-16 20:33:59 -0500

    t123

--- Selenium_Automation/src/corejava/GitDemoBranch.java
+++ Selenium_Automation/src/corejava/GitDemoBranch.java
@@ -4,7 +4,7 @@ public class GitDemoBranch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Move code to Suganya Branch*****");
+		System.out.println("Move code to Suganya Branch");
 	}
 
 }
